1b4b1ad46b0157e99d04a87cda58bf3af5ec67ea
Jehú <jehu.sagardoy@gmail.com>
2019-11-12 19:37:52 +0100

    codesandbox

--- src/scenes/emisores.scene.tsx
+++ src/scenes/emisores.scene.tsx
@@ -4,12 +4,12 @@ import { Column } from 'material-table';
 import { TableComponent } from '@pods';
 import Checkbox from '@material-ui/core/Checkbox/Checkbox';
 
-export const EmisoresScene = () => {
+export const EmisoresScene: React.FC = () => {
     const columnsEmpresa = [
         {
             title: 'Selected',
             field: 'selected',
-            render: dataRow => <Checkbox checked={dataRow.selected} disabled={dataRow.selected && isDisabled()} onChange={e => handleCheck(dataRow)} />,
+            render: dataRow => selection({ ...dataRow }),
         },
         { title: 'Nombre', field: 'nombre' },
         { title: 'NIF', field: 'NIF' },
@@ -20,38 +20,51 @@ export const EmisoresScene = () => {
         { title: 'Pais', field: 'pais' },
         { title: 'IBAN', field: 'iban' },
     ];
-    
-    const [state, setState] = React.useState<TableState>({
-        columns: columnsEmpresa,
-        data: emisoresData,
-    });
 
-    const  isDisabled = ():boolean => {
-        const newArray =  [...state.data];
-        const res = newArray.find((item)=>item.selected===true);
-        return res?true:false;
-    }
+    const [emisores, setEmisores] = React.useState<TableState>({
+        columns: [...columnsEmpresa],
+        data: [...emisoresData],
+    });
+    React.useEffect(() => console.log(`aaaa` + emisores), [emisores]);
+
+    const selection = (dataRow: Empresa) => (
+        <Checkbox
+            checked={isChecked(dataRow)}
+            //disabled={dataRow.selected && isDisabled()}
+            onChange={e => handleCheck(dataRow)}
+        />
+    );
+
+    const isChecked = (dataRow: Empresa) => {
+        const data = [...emisores.data];
+        const value: Empresa = data.find((item: Empresa) => item.NIF === dataRow.NIF && item.selected === true);
+
+        return value ? true : false;
+    };
+
+    const isDisabled = (): boolean => {
+        const newArray = [...emisores.data];
+        const res = newArray.find(item => item.selected === true);
+        return res ? true : false;
+    };
 
     const handleCheck = (empresa: Empresa) => {
-        const newData = [...state.data];
-        const rest = newData.map(item => {
+        const newData = [...emisores.data];
+        const rest: Empresa[] = newData.map((item: Empresa) => {
             if (item.NIF === empresa.NIF) {
-                const newItem = {...item, selected: !empresa.selected}
+                const newItem = { ...item, selected: !item.selected };
                 return newItem;
             } else {
                 return item;
             }
         });
-        const newState = {...state, data:rest}
-        setState(
-            newState
-        );
-        console.log(state.data);
+        setEmisores({ ...emisores, data: rest });
     };
+
     interface TableState {
         columns: Column<Empresa>[];
         data: Empresa[];
     }
 
-    return <TableComponent title="Emisores" data={state.data} columns={state.columns} />;
+    return <TableComponent title="Emisores" data={emisores.data} columns={emisores.columns} />;
 };
